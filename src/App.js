b92de5a6310144186a081c91fac0964ab7b1110c
mahadi215 <mahadihasanbd215@gmail.com>
2022-09-29 20:10:25 +0300

    profile add

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-console.log('hello');
-console.log('hello');
-console.log('hello');
-console.log('hello');
+
 function App() {
   return (
     <div className="App">

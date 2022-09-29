43eaf20efd798f3cfe6f55cd51825d6cf5ef63a2
mahadi215 <mahadihasanbd215@gmail.com>
2022-09-29 20:05:04 +0300

    func add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-
+console.log('hello');
 function App() {
   return (
     <div className="App">
